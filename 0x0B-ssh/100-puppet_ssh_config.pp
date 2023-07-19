e2c4d6053a29826005f1463c1ad6d3d6a3b29bef
keith-kay <keithrhova@gmail.com>
2023-07-19 14:30:04 -0700

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH Client configuration
+	host*
+	IdentityFile /root/alx-system_engineering-devops/0x0B-ssh/.ssh/school
+	PasswordAuthentication no
+}
