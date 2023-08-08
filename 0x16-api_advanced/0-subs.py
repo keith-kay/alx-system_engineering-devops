1e7966dec6f94a784cac1540a058bfdf06d24842
keith-kay <keithrhova@gmail.com>
2023-08-08 13:41:19 -0700

    task 0-updt

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-function that queries the Reddit API and returns the number of subscribers (not active users,
+function that queries the Reddit API and returns the number of 
+subscribers (not active users,
 total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0
 """
