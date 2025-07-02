b70de1fdb2411f15bd008200635883de0c0eb62c
keith-kay <keithrhova@gmail.com>
2025-07-02 14:56:27 +0300

    updt00

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,18 +3,36 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => @(END),
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+| END
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+  require   => Package['nginx'],
 }
