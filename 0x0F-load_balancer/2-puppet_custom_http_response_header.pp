41e93243c0a80f265dfd1f807ba40ce1ca248b4e
keith-kay <keithrhova@gmail.com>
2025-07-07 09:05:30 +0300

    task 003

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,33 +1,35 @@
-# Puppet manifest to configure Nginx with a custom HTTP response header X-Served-By
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/nginx.conf'],
-}
-
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
+  ensure  => running,
   require => Package['nginx'],
-  content => template('nginx/nginx.conf.erb'),
-}
-
-# Template file nginx.conf.erb should include:
-# add_header X-Served-By $hostname always; inside the http block
-# You can also use file_line if you want to avoid templates.
-
-file_line { 'add_custom_header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => '    add_header X-Served-By $hostname always;',
-  match => 'add_header X-Served-By',
-  after => 'http {',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
